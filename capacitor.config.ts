380cfb653e92143a38e1595041e40acb4b9fb53e
hichambifden <hicham.bifden@gmail.com>
2025-06-15 11:18:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mini-app-dashboard',
+  webDir: 'dist'
+};
+
+export default config;
